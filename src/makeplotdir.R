2c41a330ea0f858930cd1f71c8232fa514a75fa9
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2024-04-11 11:19:56 +0100

    Fix Martingale typo. Organise saved plots.

---
+++ src/makeplotdir.R
@@ -0,0 +1,70 @@
+dir.create("plots/cd/soft-flare/diet", recursive = TRUE, showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/diet", recursive = TRUE, showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/diet", recursive = TRUE, showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/diet", recursive = TRUE, showWarnings = FALSE)
+
+dir.create("plots/cd/soft-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+dir.create("plots/cd/soft-flare/biochem",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/biochem",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/biochem",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/biochem",
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+
+dir.create("plots/cd/soft-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/demographics",
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+dir.create("plots/cd/soft-flare/controlled",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/controlled",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/controlled",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/controlled",
+           recursive = TRUE,
+           showWarnings = FALSE)
+
+dir.create("plots/cd/soft-flare/ibd",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/cd/hard-flare/ibd",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/soft-flare/ibd",
+           recursive = TRUE,
+           showWarnings = FALSE)
+dir.create("plots/uc/hard-flare/ibd",
+           recursive = TRUE,
+           showWarnings = FALSE)
