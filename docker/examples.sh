850503eaafec375db3cdd23984f4121d4b9006c7
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-04-16 10:50:26 +0100

    Add renv based on Docker image

--- docker/examples.sh
+++ docker/examples.sh
@@ -2,9 +2,10 @@
 
 docker image build -t ghcr.io/nathansam/predicct .
 
-docker container run -it \
-  --mount type=bind,source="/Volumes/igmm/cvallejo-predicct/predicct",target="/analysis/data" \
+docker container run -it --rm \
+  --mount type=bind,source="/Volumes/igmm/cvallejo-predicct/predicct/",target="/analysis/data" \
   --mount type=bind,source="$(pwd)/docs",target="/analysis/docs" \
+  --mount type=bind,source="$(pwd)/src",target="/analysis/src" \
   ghcr.io/nathansam/predicct
 
 docker container run  --rm \
