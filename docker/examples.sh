d20e48a421bae492d16ad3b886744c507e5277a4
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-04-21 14:26:09 +0100

    Update processed file names

--- docker/examples.sh
+++ docker/examples.sh
@@ -6,10 +6,10 @@ docker container run -it --rm \
   --mount type=bind,source="/Volumes/igmm/cvallejo-predicct/predicct/",target="/analysis/data" \
   --mount type=bind,source="$(pwd)/docs",target="/analysis/docs" \
   --mount type=bind,source="$(pwd)/src",target="/analysis/src" \
-  ghcr.io/nathansam/predicct
+  ghcr.io/nathansam/predicct:1.0.1
 
 docker container run  --rm \
   --mount type=bind,source="/Volumes/igmm/cvallejo-predicct/predicct/",target="/analysis/data" \
   --mount type=bind,source="$(pwd)/docs",target="/analysis/docs" \
   --mount type=bind,source="$(pwd)/src",target="/analysis/src" \
-  ghcr.io/nathansam/predicct
+  ghcr.io/nathansam/predicct:1.0.1
