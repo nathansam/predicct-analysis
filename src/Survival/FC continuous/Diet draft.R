2f24d6b5b4cb34352136ab06e5467da8b36f147c
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-10-17 11:42:55 +0100

    Calculation of population cumulative incidence

--- src/Survival/FC continuous/Diet draft.R
+++ src/Survival/FC continuous/Diet draft.R
@@ -7,41 +7,11 @@ library(splines)
 # Version of Diet with FC as a continuous variable
 
 # Load data
-# Run first chunk of Diet.qmd
-
-# FC has been logged twice - reverse
-flare.uc.df$FC <- exp(flare.uc.df$FC)
-# So now FC is the log of the FC measurement
+# Run setting up in R in my version of Diet.qmd
 
 # UPF
 
 # Survival analysis
-
-# Original
-coxph(Surv(softflare_time, softflare) ~
-    Sex + cat + IMD + dqi_tot + UPF_perc + frailty(SiteNo),
-  control = coxph.control(outer.max = 20),
-  data = flare.uc.df
-) %>%
-  broom::tidy(exp = TRUE, conf.int = TRUE)
-
-
-# New
-
-# Remove IMD
-
-# Add Age - in decades so coefficients are easier to interpret/scale is better
-# Zero at age 40
-flare.uc.df %<>%
-  dplyr::mutate(
-    age_decade = Age/10
-  )
-
-# DQI scaled
-
-
-# Add BMIcat
-# Add FC using a spline
 
 cox <- coxph(Surv(softflare_time, softflare) ~
                Sex + 
@@ -77,121 +47,13 @@ cox_null %>%
 # Likelihood ratio test
 anova(cox, cox_null, test = 'LRT')
 
-# Function to perform the LRT
-summon_lrt <- function(model, term) {
-  # Function that removes a term and performs a
-  # Likelihood ratio test to determine its significance
-  
-  # Rename term to avoid problems
-  term_removed <- term
-  
-  update(model, paste0("~ . - ", term_removed)) %>%
-    anova(model, ., test = 'LRT') %>%
-    broom::tidy() %>%
-    dplyr::filter(!is.na(p.value)) %>%
-    dplyr::mutate(term = term_removed) %>%
-    dplyr::mutate(test = 'LRT') %>%
-    dplyr::relocate(term) %>%
-    dplyr::relocate(test, .after = term)
-  
-}
 
-summon_lrt(cox, term = "ns(UPF_perc, df = 2)")
+summon_lrt(cox, remove = "ns(UPF_perc, df = 2)")
 
 
-
-
-
-# Plotting the shape of age
+# Plotting the shape of continuous variables
 # Dummy data
 # Reference values
-
-summon_reference_values <- function(data, variables){
-  # Function that extracts the reference value for each variable in a data frame
-  # Reference is either the first factor level or the median
-  # Used for producing HR plots
-  
-  data %>%
-    dplyr::select(tidyselect::all_of(variables)) %>%
-    dplyr::summarise(across(
-      .cols = everything(),
-      .fns = function(x) {
-        if (is.factor(x)) {
-          levels(x) %>% dplyr::first()
-        } else if (is.numeric(x)) {
-          median(x, na.rm = TRUE)
-        } else {
-          NULL
-        }
-      }
-    ))
-  
-}
-
-
-plot_continuous_hr <- function(data, model, variable){
-  # Plotting continuous Hazard Ratio curves
-  
-  # model is a Cox model
-  # variable is the variable we are plotting
-  
-  # Range of the variable to plot
-  variable_range <- data %>%
-    dplyr::pull(variable) %>%
-    quantile(., probs = seq(0, 0.9, 0.01), na.rm = TRUE) %>%
-    unname() %>%
-    unique()
-  
-  # Reference variables
-  # Extract variables used in the Cox model
-  # Remove the variable we want to vary and the SiteNo
-  ref_variables <- all.vars(delete.response(terms(model))) %>%
-    tibble(x = .) %>%
-    dplyr::filter(
-      x != variable,
-      x != 'SiteNo'
-    ) %>%
-    dplyr::pull(x)
-  
-  # Reference level of the continuous variable
-  # Set to the lowest
-  ref_value <- variable_range %>% min()
-  
-  # Create dummy data for plotting
-  # Reference levels
-  data %>%
-    summon_reference_values(
-      data = .,
-      variables = ref_variables
-    ) %>% {
-    # Creating dummy data
-      tibble(
-        .,
-        !!sym(variable) := variable_range
-      )
-    } %>% {
-      pred = predict(model, newdata = ., type = "lp", se = TRUE)
-      
-      tibble(., p = pred$fit, se = pred$se)
-    } %>% {
-      df <- .
-      
-      # Subtract chosen reference value to set linear predictor to 0 at that value
-      denominator <- df %>% 
-        dplyr::filter(!!sym(variable) == ref_value) %>%
-        dplyr::pull(p)
-    
-      df %>% 
-        dplyr::mutate(p = p - denominator)
-    
-    } %>%
-    ggplot(aes(x = !!rlang::sym(variable), y = exp(p), ymin = exp(p - 1.96*se), ymax = exp(p + 1.96*se))) +
-    geom_point() +
-    geom_line() +
-    geom_ribbon(alpha = 0.2) +
-    ylab("HR")
-  
-}
 
 # Age
 plot_continuous_hr(
@@ -215,3 +77,75 @@ plot_continuous_hr(
 )
 
 
+# Absolute risk difference in the population, defined as
+# Expected probability of event occurring if entire population was unexposed
+# Minus the expected probability of event occurring if entire population was exposed
+
+# For continuous variables, I guess we should choose some values to estimate at
+# Ironically, I think I will probably choose quantiles
+
+
+summon_population_cum_incidence <- function(data, model, times, variable, values) {
+  # Function that calculates mean population cumulative incidence at a given time
+  # with a specified value of the variable
+  # using a Cox model
+  
+  tidyr::expand_grid(times, values) %>%
+    {
+      purrr::map2_dfr(
+        .x = .$times,
+        .y = .$values,
+        .f = function(time, value) {
+          data %>%
+            dplyr::mutate(softflare_time = time, !!sym(variable) := value) %>%
+            # Estimate expected for entire population
+            predict(model, newdata = ., type = 'expected') %>%
+            tibble(expected = .) %>%
+            # Remove NA
+            dplyr::filter(!is.na(expected)) %>%
+            # Calculate survival and cumulative incidence (1 - survival)
+            dplyr::mutate(survival = exp(-expected),
+                          cum_incidence = 1 - survival) %>%
+            dplyr::select(cum_incidence) %>% {
+              df = .
+              
+              # Bootstrapping for confidence intervals
+              nboot = 100
+              
+              1:nboot %>%
+                purrr::map_dfr(
+                  .f = function(x) {
+                    df %>%
+                      dplyr::slice_sample(prop = 1, replace = TRUE) %>%
+                      dplyr::summarise(cum_incidence_mean_boot = mean(cum_incidence))
+                  }
+                )
+            } %>%
+            dplyr::summarise(
+              cum_incidence_mean = mean(cum_incidence_mean_boot),
+              conf.low = quantile(cum_incidence_mean_boot, 0.025),
+              conf.high = quantile(cum_incidence_mean_boot, 0.975)
+            ) %>%
+            dplyr::mutate(time = time, !!sym(variable) := value)
+        }
+      )
+    } %>%
+    dplyr::mutate(!!sym(variable) := forcats::as_factor(!!sym(variable)))
+  
+}
+
+# Use age_decade for illustration
+
+# Set softflare_time
+# Set age_decade, which we will vary
+times = seq(from = 0, to = 730, by = 182.5)
+variable = 'age_decade'
+values = c(2.5, 3.5, 4.5, 5.5, 6.5, 7.5)
+
+summon_population_cum_incidence(
+  flare.uc.df,
+  cox,
+  times = times,
+  variable = variable,
+  values = values
+)
