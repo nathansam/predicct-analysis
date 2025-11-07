78458271c6e53a2d9bf89a07f587798e079a0c87
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-11-07 11:29:27 +0000

    Medications, focus on DifficultRemembering for now.

--- src/Psychosocial/Alex Rudge/medications.R
+++ src/Psychosocial/Alex Rudge/medications.R
@@ -1,26 +1,40 @@
+library(tidyverse)
+library(magrittr)
+
+
 
 # Looking at medication data
 # Mainly interested in Q25 - do you sometimes forget to take your pills?
 # Q26 also maybe interesting - miss medication for other reason?
 
-
 data.path <- "/Volumes/igmm/cvallejo-predicct/predicct/final/20221004/"
 
-data_meds <- readxl::read_xlsx(
+medications <- readxl::read_xlsx(
   paste0(data.path, "Baseline2022/medications.xlsx")
 )
 
 # Select relevant columns
-data_meds %<>%
-  dplyr::select(ParticipantNo, ForgetPills:TakeMedicineYesterday)
+medications %<>%
+  dplyr::select(ParticipantNo, SiteNo, ForgetPills:TakeMedicineYesterday, DifficultyRemembering)
+
+# Factor recode
+medications %<>%
+  dplyr::mutate(ForgetPills = forcats::as_factor(ForgetPills)) %>%
+  dplyr::mutate(ForgetPills = forcats::fct_recode(ForgetPills, Yes = '1', No = '2'))
 
 
 # Counts
 # Forgot pills
-data_meds %>%
+medications %>%
   dplyr::count(ForgetPills)
 # 1 is yes, 2 is no
 
 # Missed for other reason
-data_meds %>%
+medications %>%
   dplyr::count(MissMedication)
+
+# How often do you have difficult remembering to take all your medicine
+medications %>%
+  dplyr::count(DifficultyRemembering)
+# Seems to be a better version of ForgetPills
+
