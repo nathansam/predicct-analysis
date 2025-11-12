360a6cd369595bd65a3a66b371b399137f265441
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-11-12 14:53:25 +0000

    Baseline tables exercise created.

---
+++ src/Psychosocial/Alex Rudge/Baseline tables Exercise.R
@@ -0,0 +1,72 @@
+library(gt)
+library(gtsummary)
+
+# Variable to include
+variables = c('age',
+              'Sex',
+              'flare_group',
+              'FC',
+              'Smoke',
+              'OverallControl')
+
+# Tidy up the data ready for the table
+data_baseline_table <- data_baseline %>%
+  dplyr::mutate(
+    diagnosis2 = dplyr::case_match(
+      diagnosis2,
+      "CD" ~ "Crohn's Disease",
+      "UC/IBDU" ~ 'Ulcerative Colitis'
+    ) 
+  ) %>%
+  # Unlog FC
+  dplyr::mutate(FC = exp(FC)) %>%
+  # Reword Flares
+  dplyr::mutate(
+    flare_group = forcats::fct_recode(
+      flare_group,
+      "None" = "No flares",
+      "At least one" = "1 or More Flares"
+  ))
+  
+
+data_baseline_table %>%
+  gtsummary::tbl_strata(
+    strata = diagnosis2,
+    .tbl_fun = ~
+      .x %>%
+      gtsummary::tbl_summary(
+        by = MinimumExercise,
+        include = variables,
+        missing_text = 'Missing data',
+        label = list(
+          age ~ "Age",
+          flare_group ~ "Flares in previous year",
+          FC ~ 'Fecal Calprotectin',
+          Smoke ~ "Smoking",
+          OverallControl ~ "VAS Control Score"
+        )
+      ) %>%
+      gtsummary::add_p() %>%
+      gtsummary::add_q(method = 'holm') %>%
+      gtsummary::bold_p(q = TRUE),
+    .header = "**{strata}**, N = {n}"
+  ) %>%
+  gtsummary::as_gt() %>%
+  gt::tab_spanner(
+    label = gt::md("**Minimum Recommended Exercise**"),
+    columns = c(stat_1_1, stat_2_1, stat_1_2, stat_2_2),
+    level = 2,
+    gather = FALSE
+  ) %>%
+  {tbl <- .
+  
+  # Swap spanner hierarchy
+  tbl$`_spanners` <- tbl$`_spanners` %>%
+    dplyr::mutate(spanner_level =
+    dplyr::case_match(
+      spanner_level,
+      1 ~ 2,
+      2 ~ 1))
+   
+  tbl
+  }
--- src/Psychosocial/Alex Rudge/Baseline tables HADS.R
+++ src/Psychosocial/Alex Rudge/Baseline tables HADS.R
@@ -10,7 +10,6 @@ variables = c('age',
               'OverallControl')
 
 # Tidy up the data ready for the table
-
 data_baseline_anxiety_table <- data_baseline_anxiety %>%
   dplyr::mutate(
     diagnosis2 = dplyr::case_match(
@@ -29,26 +28,107 @@ data_baseline_anxiety_table <- data_baseline_anxiety %>%
       "At least one" = "1 or More Flares"
   ))
 
-
+# Anxiety
 data_baseline_anxiety_table %>%
   gtsummary::tbl_strata(
     strata = diagnosis2,
     .tbl_fun = ~
       .x %>%
       gtsummary::tbl_summary(
-        by = 'score_group',
-        include = independent,
+        by = score_group,
+        include = variables,
         missing_text = 'Missing data',
         label = list(
           age ~ "Age",
           flare_group ~ "Flares in previous year",
           FC ~ 'Fecal Calprotectin',
-          Smoke ~ "Smoker",
+          Smoke ~ "Smoking",
           OverallControl ~ "VAS Control Score"
         )
       ) %>%
       gtsummary::add_p() %>%
       gtsummary::add_q(method = 'holm') %>%
-      gtsummary::modify_spanning_header(c("stat_1", "stat_2", "stat_3") ~ "**HADS Anxiety Score**"),
+      gtsummary::bold_p(q = TRUE),
     .header = "**{strata}**, N = {n}"
-  )
+  ) %>%
+  gtsummary::as_gt() %>%
+  gt::tab_spanner(
+    label = gt::md("**HADS Anxiety Score**"),
+    columns = c(stat_1_1, stat_2_1, stat_3_1, stat_1_2, stat_2_2, stat_3_2),
+    level = 2,
+    gather = FALSE
+  ) %>%
+  {tbl <- .
+  
+  # Swap spanner hierarchy
+  tbl$`_spanners` <- tbl$`_spanners` %>%
+    dplyr::mutate(spanner_level =
+    dplyr::case_match(
+      spanner_level,
+      1 ~ 2,
+      2 ~ 1))
+   
+  tbl
+  }
+
+# Depression
+# Tidy up the data ready for the table
+data_baseline_depression_table <- data_baseline_depression %>%
+  dplyr::mutate(
+    diagnosis2 = dplyr::case_match(
+      diagnosis2,
+      "CD" ~ "Crohn's Disease",
+      "UC/IBDU" ~ 'Ulcerative Colitis'
+    ) 
+  ) %>%
+  # Unlog FC
+  dplyr::mutate(FC = exp(FC)) %>%
+  # Reword Flares
+  dplyr::mutate(
+    flare_group = forcats::fct_recode(
+      flare_group,
+      "None" = "No flares",
+      "At least one" = "1 or More Flares"
+    ))
+
+data_baseline_depression_table %>%
+  gtsummary::tbl_strata(
+    strata = diagnosis2,
+    .tbl_fun = ~
+      .x %>%
+      gtsummary::tbl_summary(
+        by = score_group,
+        include = variables,
+        missing_text = 'Missing data',
+        label = list(
+          age ~ "Age",
+          flare_group ~ "Flares in previous year",
+          FC ~ 'Fecal Calprotectin',
+          Smoke ~ "Smoking",
+          OverallControl ~ "VAS Control Score"
+        )
+      ) %>%
+      gtsummary::add_p() %>%
+      gtsummary::add_q(method = 'holm') %>%
+      gtsummary::bold_p(q = TRUE),
+    .header = "**{strata}**, N = {n}"
+  ) %>%
+  gtsummary::as_gt() %>%
+  gt::tab_spanner(
+    label = gt::md("**HADS Depression Score**"),
+    columns = c(stat_1_1, stat_2_1, stat_3_1, stat_1_2, stat_2_2, stat_3_2),
+    level = 2,
+    gather = FALSE
+  ) %>%
+  {tbl <- .
+  
+  # Swap spanner hierarchy
+  tbl$`_spanners` <- tbl$`_spanners` %>%
+    dplyr::mutate(spanner_level =
+                    dplyr::case_match(
+                      spanner_level,
+                      1 ~ 2,
+                      2 ~ 1))
+  
+  tbl
+  }
