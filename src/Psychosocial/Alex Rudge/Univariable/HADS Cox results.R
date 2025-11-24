0a6836ca16f3aab5f8be727ee59de20c20fb4821
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-11-24 15:39:15 +0000

    Renamed to univariable

---
+++ src/Psychosocial/Alex Rudge/Univariable/HADS Cox results.R
@@ -0,0 +1,93 @@
+
+# Extracting the results from the Cox models
+
+# Run HADS
+
+# Anxiety
+# Extract Cox results
+cox_results_hads_anxiety_cc <- extract_cox_results(
+  data = data_survival_anxiety_soft_uc,
+  cox_model = cox_anxiety_soft_uc,
+  flare_type = 'soft',
+  diagnosis2 = 'UC/IBDU',
+  variable = 'score_group'
+) %>%
+  dplyr::bind_rows(
+    extract_cox_results(
+      data = data_survival_anxiety_soft_cd,
+      cox_model = cox_anxiety_soft_cd,
+      flare_type = 'soft',
+      diagnosis2 = 'CD',
+      variable = 'score_group'
+    )
+  ) %>%
+  dplyr::bind_rows(
+    extract_cox_results(
+      data = data_survival_anxiety_hard_uc,
+      cox_model = cox_anxiety_hard_uc,
+      flare_type = 'hard',
+      diagnosis2 = 'UC/IBDU',
+      variable = 'score_group'
+    ) %>%
+      dplyr::bind_rows(
+        extract_cox_results(
+          data = data_survival_anxiety_hard_cd,
+          cox_model = cox_anxiety_hard_cd,
+          flare_type = 'hard',
+          diagnosis2 = 'CD',
+          variable = 'score_group'
+        )
+      )
+  )
+
+
+# Depression
+cox_results_hads_depression_cc <- extract_cox_results(
+  data = data_survival_depression_soft_uc,
+  cox_model = cox_depression_soft_uc,
+  flare_type = 'soft',
+  diagnosis2 = 'UC/IBDU',
+  variable = 'score_group'
+) %>%
+  dplyr::bind_rows(
+    extract_cox_results(
+      data = data_survival_depression_soft_cd,
+      cox_model = cox_depression_soft_cd,
+      flare_type = 'soft',
+      diagnosis2 = 'CD',
+      variable = 'score_group'
+    )
+  ) %>%
+  dplyr::bind_rows(
+    extract_cox_results(
+      data = data_survival_depression_hard_uc,
+      cox_model = cox_depression_hard_uc,
+      flare_type = 'hard',
+      diagnosis2 = 'UC/IBDU',
+      variable = 'score_group'
+    ) %>%
+      dplyr::bind_rows(
+        extract_cox_results(
+          data = data_survival_depression_hard_cd,
+          cox_model = cox_depression_hard_cd,
+          flare_type = 'hard',
+          diagnosis2 = 'CD',
+          variable = 'score_group'
+        )
+      )
+  )
+
+
+
+# Save
+filepath <- "/Volumes/igmm/cvallejo-predicct/people/Alex/Predicct2/Data/Univariable/"
+
+# readr::write_rds(
+#   x = cox_results_hads_anxiety_cc,
+#   file = paste0(filepath, "cox_results_hads_anxiety_cc.rds")
+# )
+# 
+# readr::write_rds(
+#   x = cox_results_hads_depression_cc,
+#   file = paste0(filepath, "cox_results_hads_depression_cc.rds")
+# )
