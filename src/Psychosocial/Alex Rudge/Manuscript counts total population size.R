b84ff5d8d4c232e68de081b5698b77b86433dee7
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-11-28 15:36:18 +0000

    Total number of unique patients included in the study.

---
+++ src/Psychosocial/Alex Rudge/Manuscript counts total population size.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(magrittr)
+
+
+# Manuscript counts
+
+# Study population size
+# Hard to give a single figure because we sometimes exclude depending on the
+# exposure we are using. Unless I run all psychosocial variables and find the
+# number of unique participant numbers included in the study.
+
+# Run all data cleaning
+
+hads %>%
+  dplyr::bind_rows(exercise) %>%
+  dplyr::bind_rows(lifeevents) %>%
+  dplyr::bind_rows(phq) %>%
+  dplyr::bind_rows(psqi) %>%
+  dplyr::group_by(diagnosis2) %>%
+  dplyr::distinct(ParticipantNo) %>%
+  dplyr::group_by(diagnosis2) %>%
+  dplyr::summarise(
+    n = n()
+  )
+
+# 1855 participants sufficiently answered at least one psychosocial variable.  
+# CD 935, UC/IBDU 920
+
+
