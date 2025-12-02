d566f35ce0781d18d6824bfcb103ccd8c3288cae
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-12-02 16:55:00 +0000

    Saving a df of patients who contribute to at least 1 analysis

--- src/Psychosocial/Alex Rudge/Manuscript counts total population size.R
+++ src/Psychosocial/Alex Rudge/Manuscript counts total population size.R
@@ -11,19 +11,28 @@ library(magrittr)
 
 # Run all data cleaning
 
-hads %>%
+participants <- hads %>%
   dplyr::bind_rows(exercise) %>%
   dplyr::bind_rows(lifeevents) %>%
   dplyr::bind_rows(phq) %>%
   dplyr::bind_rows(psqi) %>%
   dplyr::group_by(diagnosis2) %>%
   dplyr::distinct(ParticipantNo) %>%
+  dplyr::ungroup()
+
+
+participants %>%
   dplyr::group_by(diagnosis2) %>%
-  dplyr::summarise(
-    n = n()
-  )
+  summarise(n = n())
 
 # 1855 participants sufficiently answered at least one psychosocial variable.  
 # CD 935, UC/IBDU 920
 
 
+# Save participants
+filepath <- "/Volumes/igmm/cvallejo-predicct/people/Alex/Predicct2/Data/"
+
+readr::write_rds(
+  x = participants,
+  file = paste0(filepath, "participants.rds")
+)
