c108f8770a635a8dd539c510a5653fd99191e6e7
Alex Rudge <45514666+Rudgey@users.noreply.github.com>
2025-12-02 16:54:31 +0000

    Recreating table 1 of manuscript

--- src/Psychosocial/Alex Rudge/Cohort description table.R
+++ src/Psychosocial/Alex Rudge/Cohort description table.R
@@ -1,24 +1,42 @@
 library(tidyverse)
 library(magrittr)
+library(gtsummary)
 
 # Comparing the psychosocial cohort to the entire Predicct cohort
 
 
 # Load in the Predicct cohort
 data_cohort <- readr::read_rds(
-  file = "/Volumes/igmm/cvallejo-predicct/predicct/processed/flares-controlled.RDS"
+  file = "/Volumes/igmm/cvallejo-predicct/predicct/processed/demo-full.rds"
 )
 
-# Psychosocial cohort
-# I will just use the HADS cohort for now
+data_cd <- readr::read_rds(
+  file = "/Volumes/igmm/cvallejo-predicct/predicct/processed/demo-cd.rds"
+)
 
-# So run HADS data cleaning
+data_uc <- readr::read_rds(
+  file = "/Volumes/igmm/cvallejo-predicct/predicct/processed/demo-uc.rds"
+)
+
+# Control scores
+data_control <- readr::read_rds(
+  file = "/Volumes/igmm/cvallejo-predicct/people/chiara/IBD_C.rds"
+)
+
+
+# Psychosocial cohort
+# Load participants
+
+participants <- readr::read_rds(
+  file = "/Volumes/igmm/cvallejo-predicct/people/Alex/Predicct2/Data/participants.rds"
+)
 
 # Select relevant columns
 data_cohort %<>%
   dplyr::select(
     ParticipantNo,
     SiteNo,
+    diagnosis,
     diagnosis2,
     Sex,
     Age,
@@ -27,19 +45,23 @@ data_cohort %<>%
     IMD,
     `IBD Duration`,
     Treatment,
+    Biologic,
     Smoke,
     FC,
-    softflare,
-    softflare_time,
-    hardflare,
-    hardflare_time
+    CReactiveProtein
+  )
+
+# Rename IBD duration
+data_cohort %<>%
+  dplyr::rename(
+    IBD_duration = `IBD Duration`
   )
 
 # Flag if a patient is in the psychosocial cohort
 data_cohort %<>%
   dplyr::mutate(
     psychosocial = dplyr::case_when(
-      ParticipantNo %in% hads$ParticipantNo ~ 'Yes',
+      ParticipantNo %in% participants$ParticipantNo ~ 'Yes',
       .default = 'No'
     )
   )
@@ -49,75 +71,303 @@ data_cohort %>%
   dplyr::count(psychosocial)
 
 
-# Is missing being in the psychosocial cohort informative of flare?
-
-data_cohort_soft_uc <- data_cohort %>%
-  dplyr::filter(diagnosis2 == 'UC/IBDU') %>%
-  dplyr::rename(DiseaseFlareYN = softflare, time = softflare_time)
-
-data_cohort_soft_cd <- data_cohort %>%
-  dplyr::filter(diagnosis2 == 'CD') %>%
-  dplyr::rename(DiseaseFlareYN = softflare, time = softflare_time)
-
-legend.title = 'Responded to questionnaire'
-legend.labs = c('No', 'Yes')
-okabe_ito <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-palette = okabe_ito
-dependent = 'psychosocial'
+# Control score
+data_cohort %<>%
+  dplyr::left_join(
+    data_control %>%
+      dplyr::select(ParticipantNo, OverallControl, control_8),
+    by = "ParticipantNo"
+  )
 
 
-# Soft
-# UC
-summon_km_curves(
-  data = data_cohort_soft_uc,
-  dependent = dependent,
-  title = "Time to patient reported flare in ulcerative colitis",
-  legend.title = legend.title,
-  legend.labs = legend.labs,
-  palette = palette
+# Trick to compare entire cohort to a subset
+data_table <- dplyr::bind_rows(
+  data_cohort %>%
+  dplyr::mutate(cohort = 'predicct'),
+  data_cohort %>%
+    dplyr::filter(psychosocial == 'Yes') %>%
+    dplyr::mutate(cohort = 'psychosocial')
 )
 
-# CD
-summon_km_curves(
-  data = data_cohort_soft_cd,
-  dependent = dependent,
-  title = "Time to patient reported flare in Crohn's disease",
-  legend.title = legend.title,
-  legend.labs = legend.labs,
-  palette = palette
+# Tidy up variables
+# IBD
+data_table %<>%
+  dplyr::mutate(IMD = as.character(IMD)) %>%
+  dplyr::mutate(
+    IMD = dplyr::case_match(
+      IMD,
+      '1' ~ '1 (most deprived)',
+      '2' ~ '2',
+      '3' ~ '3',
+      '4' ~ '4',
+      '5' ~ '5 (least deprived)'
+    )
+  )
+
+# Cohort
+data_table %<>%
+  dplyr::mutate(
+    cohort = dplyr::case_match(
+      cohort,
+      'predicct' ~ 'Total PREdiCCt Cohort',
+      'psychosocial' ~ 'Psychosocial Cohort'
+    )
+  ) %>%
+  dplyr::mutate(
+    cohort = factor(cohort),
+    cohort = forcats::fct_relevel(cohort, 'Total PREdiCCt Cohort')
+  )
+
+# Table
+variables <- c(
+  'Age',
+  'Sex',
+  'Ethnicity',
+  'BMI',
+  'IMD',
+  'diagnosis',
+  'IBD_duration',
+  'control_8',
+  'OverallControl',
+  'FC',
+  'CReactiveProtein',
+  'Biologic'
 )
 
-# Very - needs mentioning in the paper.
+data_table %>%
+  gtsummary::tbl_summary(
+    by = cohort,
+    include = variables,
+    missing_text = 'Missing data',
+    label = list(
+      Age ~ "Age",
+      Sex ~ 'Sex',
+      Ethnicity ~ "Ethnicity",
+      IMD ~ 'Index of multiple deprivation',
+      diagnosis ~ 'IBD Type',
+      IBD_duration ~ 'IBD Duration',
+      control_8 ~ 'IBD-Control-8',
+      OverallControl ~ 'IBD-Control-VAS',
+      FC ~ 'Fecal calprotectin (ug/g)',
+      CReactiveProtein ~ 'C-reactive protein (mg/L)',
+      Biologic ~ 'Biologic use'
+    )
+  )
 
 
-# Hard flare
-data_cohort_hard_uc <- data_cohort %>%
-  dplyr::filter(diagnosis2 == 'UC/IBDU') %>%
-  dplyr::rename(DiseaseFlareYN = hardflare, time = hardflare_time)
-
-data_cohort_hard_cd <- data_cohort %>%
-  dplyr::filter(diagnosis2 == 'CD') %>%
-  dplyr::rename(DiseaseFlareYN = hardflare, time = hardflare_time)
 
 
-# UC
-summon_km_curves(
-  data = data_cohort_hard_uc,
-  dependent = dependent,
-  title = "Time to objective flare in ulcerative colitis",
-  legend.title = legend.title,
-  legend.labs = legend.labs,
-  palette = palette
+# Crohn's specific variables
+
+data_cd %<>%
+  dplyr::select(
+    ParticipantNo,
+    Location,
+    Behaviour,
+    Perianal,
+    HBI,
+    Surgery,
+    Smoke,
+    Biologic
+)
+  
+# Flag if a patient is in the psychosocial cohort
+data_cd %<>%
+  dplyr::mutate(
+    psychosocial = dplyr::case_when(
+      ParticipantNo %in% participants$ParticipantNo ~ 'Yes',
+      .default = 'No'
+    )
+  )
+
+# Trick to compare entire cohort to a subset
+data_table_cd <- dplyr::bind_rows(
+  data_cd %>%
+    dplyr::mutate(cohort = 'predicct'),
+  data_cd %>%
+    dplyr::filter(psychosocial == 'Yes') %>%
+    dplyr::mutate(cohort = 'psychosocial')
 )
 
-# CD
-summon_km_curves(
-  data = data_cohort_hard_cd,
-  dependent = dependent,
-  title = "Time to objective flare in Crohn's disease",
-  legend.title = legend.title,
-  legend.labs = legend.labs,
-  palette = palette
+# Tidy up variables
+# Harvey Bradshaw as categorical
+data_table_cd %<>%
+  dplyr::mutate(
+    HBI_cat = dplyr::case_when(
+      HBI < 5 ~ '<5',
+      HBI %in% c(5, 6, 7) ~ '5-7',
+      (HBI >= 8) & (HBI <= 16) ~ '8-16',
+      HBI > 16 ~ '>16'
+    )) %>%
+  dplyr::mutate(HBI_cat = factor(HBI_cat)) %>%
+  dplyr::mutate(HBI_cat = forcats::fct_relevel(
+    HBI_cat,
+    '<5', '5-7', '8-16', '>16'
+  ))
+
+# Cohort
+data_table_cd %<>%
+  dplyr::mutate(
+    cohort = dplyr::case_match(
+      cohort,
+      'predicct' ~ 'Total PREdiCCt Cohort',
+      'psychosocial' ~ 'Psychosocial Cohort'
+    )
+  ) %>%
+  dplyr::mutate(
+    cohort = factor(cohort),
+    cohort = forcats::fct_relevel(cohort, 'Total PREdiCCt Cohort')
+  )
+
+variables_cd <- c(
+  'Location',
+  'Behaviour',
+  'Perianal',
+  'HBI_cat',
+  'Surgery',
+  'Smoke',
+  'Biologic'
 )
 
+data_table_cd %>%
+  gtsummary::tbl_summary(
+    by = cohort,
+    include = variables_cd,
+    missing_text = 'Missing data',
+    label = list(
+      Location ~ 'Montreal location',
+      Behaviour ~ 'Montreal behaviour',
+      Perianal ~ 'Perianal disease',
+      HBI_cat ~ 'Harvey-Bradshaw Index',
+      Surgery ~ 'Previous surgery for Crohn’s disease',
+      Smoke ~ 'Smoking status (CD)',
+      Biologic ~ 'Biologic use (CD)'
+    )
+  )
 
+
+# UC specific variables
+
+data_uc %<>%
+  dplyr::select(
+    ParticipantNo,
+    Extent, 
+    Mayo,
+    Smoke,
+    Biologic
+  )
+
+# Flag if a patient is in the psychosocial cohort
+data_uc %<>%
+  dplyr::mutate(
+    psychosocial = dplyr::case_when(
+      ParticipantNo %in% participants$ParticipantNo ~ 'Yes',
+      .default = 'No'
+    )
+  )
+
+# Trick to compare entire cohort to a subset
+data_table_uc <- dplyr::bind_rows(
+  data_uc %>%
+    dplyr::mutate(cohort = 'predicct'),
+  data_uc %>%
+    dplyr::filter(psychosocial == 'Yes') %>%
+    dplyr::mutate(cohort = 'psychosocial')
+)
+
+# Tidy up variables
+
+# Partial Mayo as categorical
+data_table_uc %<>%
+  dplyr::mutate(
+    Mayo_cat = dplyr::case_when(
+    Mayo %in% c(0, 1) ~ '0-1',
+    Mayo %in% c(2, 3, 4) ~ '2-4',
+    Mayo %in% c(5, 6) ~ '5-6',
+    Mayo %in% c(7, 8, 9) ~ '7-9'
+    
+  ))
+
+# Cohort
+data_table_uc %<>%
+  dplyr::mutate(
+    cohort = dplyr::case_match(
+      cohort,
+      'predicct' ~ 'Total PREdiCCt Cohort',
+      'psychosocial' ~ 'Psychosocial Cohort'
+    )
+  ) %>%
+  dplyr::mutate(
+    cohort = factor(cohort),
+    cohort = forcats::fct_relevel(cohort, 'Total PREdiCCt Cohort')
+  )
+
+variables_cd <- c(
+  'Extent', 
+  'Mayo_cat',
+  'Smoke',
+  'Biologic'
+)
+
+data_table_uc %>%
+  gtsummary::tbl_summary(
+    by = cohort,
+    include = variables_cd,
+    missing_text = 'Missing data',
+    label = list(
+      Extent ~ 'Montreal extent',
+      Mayo_cat ~ 'Partial Mayo score',
+      Smoke ~ 'Smoking status (UC)',
+      Biologic ~ 'Biologic use (UC)'
+    )
+  )
+
+
+
+# Try all together
+data_table_all <- data_table %>%
+  dplyr::left_join(
+    data_table_uc %>%
+      dplyr::select(-Smoke, -Biologic, -psychosocial), by = c('ParticipantNo', 'cohort')) %>%
+  dplyr::left_join(
+    data_table_cd %>%
+      dplyr::select(-Smoke, -Biologic, -psychosocial), by = c('ParticipantNo', 'cohort'))
+
+variables_all <- c(
+  variables, 
+  'Location',
+  'Behaviour',
+  'Perianal',
+  'HBI_cat',
+  'Surgery',
+  'Extent', 
+  'Mayo_cat'
+  )
+
+tbl <- data_table_all %>%
+  gtsummary::tbl_strata(
+    strata = cohort,
+    .tbl_fun = ~
+      .x %>% 
+      gtsummary::tbl_summary(
+        by = diagnosis2,
+        include = variables_all,
+        missing_text = 'Missing data'
+      )
+  ) 
+
+tbl$table_body %<>%
+  dplyr::mutate(
+    dplyr::across(
+      .cols = c(stat_1_1, stat_2_1, stat_1_2, stat_2_2),
+      .fns = function(x) {
+        dplyr::case_match(
+          x,
+          '0 (NA%)' ~ '-',
+          .default = x
+        )
+      }
+    )
+  )
+
+tbl
