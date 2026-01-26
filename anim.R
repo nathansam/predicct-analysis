7f73f0523770de157c7d5939850c9b7dd3745f7c
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2026-01-26 11:01:49 +0000

    Add spline smoothing and enhance plot styling
    Applied a natural spline model to smooth cumulative recruitment data and updated the plot to use the model's predictions. Improved plot aesthetics with larger line size, customized axis and title text, and adjusted animation rendering dimensions.

--- anim.R
+++ anim.R
@@ -1,24 +1,43 @@
 library(gganimate)
+library(splines)
+
+test <- lm(cumulative_count~ ns(entry_date, 3), data = demo_cumulative)
+
+data.model <- data.frame(entry_date = seq(min(demo_cumulative$entry_date),
+                             max(demo_cumulative$entry_date),
+                             by = "1 day"))
+
+data.model$cumulative_count <- predict(test,
+                                    newdata = data.model)
 
 
-p <- demo_cumulative %>%
+p <- data.model %>%
   ggplot(aes(x = entry_date, y = cumulative_count)) +
   geom_line(color = rgb(34, 122, 145, maxColorValue = 255),
-            size = 1.2) +
+            size = 3) +
   theme_minimal() +
   xlab("Year") +
   ylab("Study recruitment") +
+  ggtitle("Cumulative Recruitment to PREdiCCt Over Time") +
   xlim(as.Date("2016-11-01"), as.Date("2020-04-01")) +
   scale_y_continuous(breaks = seq(0, 3000, by = 500), limits = c(0, 3000)) +
   theme(text = element_text(color = "#1C285A"),
-        axis.text = element_text(face = "bold", color = "#1C285A"))
+        axis.text = element_text(size = 20, face = "bold", color = "#1C285A"),
+        axis.title = element_text(size = 28,
+                                  face = "bold",
+                                  color = "#1C285A"),
+        plot.title = element_text(size = 28,
+                             face = "bold",
+                             color = "#1C285A",
+                             hjust = 0.5,
+                             vjust = 2))
 
 
 anim <- p +  transition_reveal(entry_date, keep_last=FALSE)
 
 an <- animate(anim, renderer =  av_renderer(file = "test.mp4"),
-              width=16 * 3/4,
-              height=9 * 3/4,
+              width= 16,
+              height= 9,
               units = "in",
               res = 300,
               fps = 25)
